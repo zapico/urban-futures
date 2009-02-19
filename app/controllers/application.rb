5bf529e2b484800bdabffedcc00205abad212874
Jorge Luis Zapico Lamela <zapico@zapico-mb.nada.kth.se>
2009-02-19 11:01:43 +0100

    heroku try

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,13 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   
+  
+  def login_required
+      unless User.find_by_id(session[:user_id])
+          flash[:notice] = 'Please log in'
+          redirect_to(:controller => 'articles', :action => 'login')
+      end
+  end
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '62a1ff068dd7cfd4c4c30708916bd909'
